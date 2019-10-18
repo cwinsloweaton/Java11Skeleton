d103becddf660072d0cd87ed360081f2234985ba
chrismac <chris@cweaton.com>
2019-10-18 10:17:08 -0700

    Assignment 1

--- Assignment1/HelloWorldApp.java
+++ Assignment1/HelloWorldApp.java
@@ -1,2 +1,15 @@
-// Student name:
-// This is where your first assignment goes!
+/**
+* The HelloWorldApp class implements an application that simply
+* displays "Hello World!" to the standard output.
+*/
+class HelloWorldApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Cat");
+        System.out.println("Eevee");
+        System.out.println(10 + 10);
+        System.out.println("Aliens!");
+        System.out.println("It's where lego comes from!");
+        
+    }
+}
